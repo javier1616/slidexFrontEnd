9a7fd3ba3ddc518b9675c15d7fdd7a23d4e2734a
javier1616 <javiersolis.unaj@gmail.com>
2025-07-06 16:45:18 -0300

    probando tunneling

--- data/config.js
+++ data/config.js
@@ -1,8 +1,15 @@
-export  const PRESENTATION_SERVICE_URL = 'https://localhost:7112/api/v1/'
-export const USER_SERVICE_URL = 'https://localhost:59542/'
-export const SESSION_SERVICE_URL = 'https://localhost:6662/api/v1/'
+//export  const PRESENTATION_SERVICE_URL = 'https://localhost:7112/api/v1/'
+//export const USER_SERVICE_URL = 'https://localhost:59542/'
+//export const SESSION_SERVICE_URL = 'https://localhost:6662/api/v1/'
 
-export const SIGNALR_HUB = 'https://localhost:6662/sessionHub/'
-export const API_URL = 'https://localhost:7112/api/v1/'
+//export const SIGNALR_HUB = 'https://localhost:6662/sessionHub/'
+//export const API_URL = 'https://localhost:7112/api/v1/'
 
 
+export const USER_SERVICE_URL = 'https://european-carpet-optional-washington.trycloudflare.com/'
+
+export const SESSION_SERVICE_URL = 'https://highly-developer-focusing-develop.trycloudflare.com/api/v1/'
+export const SIGNALR_HUB = 'https://highly-developer-focusing-develop.trycloudflare.com/sessionHub/'
+
+export const PRESENTATION_SERVICE_URL = 'https://visibility-ws-received-conclusion.trycloudflare.com/api/v1'
+export const API_URL = 'https://visibility-ws-received-conclusion.trycloudflare.com/api/v1'
