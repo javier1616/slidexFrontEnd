51af50d804205a51e91e45aeb690abe26bfee85e
javier1616 <javiersolis.unaj@gmail.com>
2025-07-06 20:40:18 -0300

    config urls

--- data/config.js
+++ data/config.js
@@ -6,10 +6,10 @@
 //export const API_URL = 'https://localhost:7112/api/v1/'
 
 
-export const USER_SERVICE_URL = 'https://prevent-q-albuquerque-mess.trycloudflare.com/'
+export const USER_SERVICE_URL = 'https://qualifying-decorative-thirty-manufacturing.trycloudflare.com/'
 
-export const SESSION_SERVICE_URL = 'https://highly-developer-focusing-develop.trycloudflare.com/api/v1/'
-export const SIGNALR_HUB = 'https://highly-developer-focusing-develop.trycloudflare.com/sessionHub/'
+export const SESSION_SERVICE_URL = 'https://chief-lp-porcelain-monroe.trycloudflare.com/api/v1/'
+export const SIGNALR_HUB = 'https://chief-lp-porcelain-monroe.trycloudflare.com/sessionHub/'
 
-export const PRESENTATION_SERVICE_URL = 'https://visibility-ws-received-conclusion.trycloudflare.com/api/v1'
-export const API_URL = 'https://visibility-ws-received-conclusion.trycloudflare.com/api/v1'
+export const PRESENTATION_SERVICE_URL = 'https://colleagues-amenities-journalism-handbook.trycloudflare.com/api/v1'
+export const API_URL = 'https://colleagues-amenities-journalism-handbook.trycloudflare.com/api/v1'
