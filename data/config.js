f039f3d2b903fa88bb77d62e0ec7b938ef24df6d
javier1616 <javiersolis.unaj@gmail.com>
2025-07-06 21:07:56 -0300

    config tunnels

--- data/config.js
+++ data/config.js
@@ -1,8 +1,23 @@
+/*
 export  const PRESENTATION_SERVICE_URL = 'https://localhost:7112/api/v1/'
 export const USER_SERVICE_URL = 'https://localhost:59542/'
 export const SESSION_SERVICE_URL = 'https://localhost:6662/api/v1/'
 
 export const SIGNALR_HUB = 'https://localhost:6662/sessionHub/'
 export const API_URL = 'https://localhost:7112/api/v1/'
+*/
 
 
+const SESSION_MS_TUNNEL = 'documents-left-clearly-styles'
+const PRESENTATION_MS_TUNNEL = 'colleagues-amenities-journalism-handbook'
+const USER_MS_TUNNEL = 'qualifying-decorative-thirty-manufacturing'
+//const HISTORY_MS_TUNNEL = ''
+
+export const USER_SERVICE_URL = `https://${USER_MS_TUNNEL}/`
+
+export  const PRESENTATION_SERVICE_URL = `https://${PRESENTATION_MS_TUNNEL}/api/v1/`
+export const API_URL = `https://${PRESENTATION_MS_TUNNEL}/api/v1/`
+
+export const SESSION_SERVICE_URL = `https://${SESSION_MS_TUNNEL}/api/v1/`
+
+export const SIGNALR_HUB = `https://${SESSION_MS_TUNNEL}/sessionHub/`
