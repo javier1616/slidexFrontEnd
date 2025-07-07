f124ca480920041737ae8ee77bbc4b0da3977206
javier1616 <javiersolis.unaj@gmail.com>
2025-07-07 17:10:13 -0300

    configuration

--- data/config.js
+++ data/config.js
@@ -1,3 +1,5 @@
+/*
+
 export const PRESENTATION_SERVICE_URL = "https://localhost:7112/api/v1/";
 export const USER_SERVICE_URL = "https://localhost:59542/";
 export const SESSION_SERVICE_URL = "https://localhost:6662/api/v1/";
@@ -8,23 +10,25 @@ export const API_URL = "https://localhost:7112/api/v1/";
 
 export const WEB_SERVER = "http://127.0.0.1:3000/";
 
+*/
+
 // Constantes de configuracion para deploy web
 
-/*
+
 export const WEB_SERVER = 'https://slidex-front-end.vercel.app/'
 
-const SESSION_MS_TUNNEL = ''
-const PRESENTATION_MS_TUNNEL = ''
-const USER_MS_TUNNEL = ''
-const HISTORY_MS_TUNNEL = ''
+const SESSION_MS_TUNNEL = 'lcd-mpegs-employed-discharge'
+const PRESENTATION_MS_TUNNEL = 'tim-iii-graph-nice'
+const USER_MS_TUNNEL = 'symphony-gba-slope-alabama'
+const HISTORY_MS_TUNNEL = 'investments-yale-orientation-assurance'
 
 export const USER_SERVICE_URL = `https://${USER_MS_TUNNEL}.trycloudflare.com/`
 
 export  const PRESENTATION_SERVICE_URL = `https://${PRESENTATION_MS_TUNNEL}.trycloudflare.com/api/v1/`
-export const API_URL = `https://${PRESENTATION_MS_TUNNEL}/api/v1/`
+export const API_URL = `https://${PRESENTATION_MS_TUNNEL}.trycloudflare.com/api/v1/`
 
 export const SESSION_SERVICE_URL = `https://${SESSION_MS_TUNNEL}.trycloudflare.com/api/v1/`
 export const SIGNALR_HUB = `https://${SESSION_MS_TUNNEL}.trycloudflare.com/sessionHub/`
 
 export const HISTORY_SERVICE_URL = `https://${HISTORY_MS_TUNNEL}.trycloudflare.com/api/History/`
-*/
+
